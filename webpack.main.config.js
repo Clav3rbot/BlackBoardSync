c5842d4c2a6a85aa02b6dd0fec1933d6ffa9e21e
Domenico Pittari <domenico.pittari@mail.polimi.it>
2026-02-23 02:10:12 +0100

    feat: detect updates by commit hash in addition to version

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,13 +1,21 @@
 const rules = require('./webpack.rules')
 const plugins = require('./webpack.plugins')
 const CopyPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack')
 const path = require('path')
+const { execSync } = require('child_process')
+
+// Get the current git commit hash at build time
+const GIT_COMMIT_HASH = execSync('git rev-parse HEAD').toString().trim()
 
 module.exports = {
     entry: './src/index.ts',
     module: { rules },
     plugins: [
         ...plugins,
+        new webpack.DefinePlugin({
+            BUILD_COMMIT_HASH: JSON.stringify(GIT_COMMIT_HASH),
+        }),
         new CopyPlugin({
             patterns: [
                 {
